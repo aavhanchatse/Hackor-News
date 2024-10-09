9c790220369e8dd0117d2703cb2cc6c132589f35
Aavhan Chatse <chatseaavhan@gmail.com>
2024-10-09 19:54:51 +0530

    Initial Commit

---
+++ Hackor News/Hackor_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hackor_NewsApp.swift
+//  Hackor News
+//
+//  Created by Aavhan Chatse on 09/10/24.
+//
+
+import SwiftUI
+
+@main
+struct Hackor_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
